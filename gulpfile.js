bb1511d82ede6f7ed85e76f3ab88482e90c5bbd3
yaorui <465964690@qq.com>
2015-11-10 15:30:03 +0800

    add new mod

--- gulpfile.js
+++ gulpfile.js
@@ -15,25 +15,34 @@ var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     uglify = require('gulp-uglify'),
     imagemin = require('gulp-imagemin'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync').create(),
+    reload = browserSync.reload;
 
 var url = {
     dev: {
         less: 'src/less/',
         css: 'src/css/',
-        img: 'src/images/',
+        font: 'src/font/',
+        img: 'src/img/',
         js: 'src/js/'
     },
+    lib: {
+        css: 'lib/css/',
+        js: 'lib/js/'
+    },
     build: {
-        css: 'Public/css',
-        img: 'Public/img',
-        js: 'Public/js'
+        css: 'Public/css/',
+        font: 'Public/font/',
+        img: 'Public/img/',
+        js: 'Public/js/'
     },
     watch: {
         less: 'src/less/*.less',
         css: 'src/css/*.css',
-        img: 'src/img/**/*.*',
-        js: 'src/js/*.js'
+        font: 'src/font/*.*',
+        img: 'src/img/*.*',
+        js: 'src/js/*.js',
+        libjs: 'lib/js/*.js'
     }
 };
 
@@ -45,7 +54,21 @@ gulp.task('less', function () {
         .pipe(mincss())
         .pipe(gulp.dest(url.build.css))
         .pipe(notify({message: 'less task complete'}))
-        .pipe(livereload());
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('font', function () {
+    gulp.src(url.dev.font + '*.*')
+        .pipe(gulp.dest(url.build.font))
+        .pipe(notify({message: 'font task complete'}))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('lib-js', function () {
+   gulp.src(url.lib.js + '*.*')
+       .pipe(gulp.dest(url.build.js))
+       .pipe(notify({message: 'js-lib task complete'}))
+       .pipe(reload({stream: true}));
 });
 
 gulp.task('script', function () {
@@ -53,42 +76,45 @@ gulp.task('script', function () {
         .pipe(plumber())
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
-        .pipe(uglify())
+        //.pipe(uglify())
         .pipe(gulp.dest(url.build.js))
-        .pipe(notify({message: 'script task complete'}));
+        .pipe(notify({message: 'script task complete'}))
+        .pipe(reload({stream: true}));
 });
 
 gulp.task('img', function () {
     gulp.src(url.dev.img + '**/*.*')
         .pipe(imagemin())
         .pipe(gulp.dest(url.build.img))
-        .pipe(notify({message: 'img task complete'}));
+        .pipe(notify({message: 'img task complete'}))
+        .pipe(reload({stream: true}));
 });
 
-gulp.task('default', ['clean', 'browser-sync'], function () {
-    gulp.run(['less', 'script', 'img', 'watch']);
+gulp.task('default', ['clean'], function () {
+    gulp.run(['less', 'lib-js', 'script', 'img', 'font', 'watch', 'browser-sync']);
 });
 
 gulp.task('clean', function () {
-   gulp.src([url.build.css, url.build.js, url.build.img])
+   gulp.src([url.build.css + '*.*', url.build.js + '*.*', url.build.img + '*.*', url.build.font + '*.*'], {read: false})
        .pipe(clean())
        .pipe(notify({message: 'clean task complete'}));
 });
 
 gulp.task('browser-sync', function() {
-    browserSync({
+    browserSync.init({
         files: "**",
         server: {
             baseDir: "./"
-        },
-        proxy: "localhost:2333"
+        }
     });
 });
 
 gulp.task('watch', function () {
-    livereload.listen();
     gulp.watch(url.watch.less, ['less']);
     gulp.watch(url.watch.img, ['img']);
+    gulp.watch(url.watch.font, ['font']);
     gulp.watch(url.watch.js, ['script']);
+    gulp.watch(url.watch.libjs, ['lib-js']);
+    gulp.watch("**/*.html").on('change', reload);
 });
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,139 @@
-var a=1;
+//页面滚动
+var scrollPage = (function (window) {
+    var $page = 0,
+        $pagenum = 0,
+        $scroll = 1,    //开启自动滚动
+        $running = 0,   //是否有动画进行中
+        $pages = {};
+
+    var init = function () {
+        $page = isNaN(location.hash.slice(1)) ? 0 : location.hash.slice(1) * 1 ;
+        $pagenum = $('.nav-li').length;
+        $page = $page >= $pagenum ? 0 : $page;
+        $('.block').each(function (i, ele) {
+            $pages[i] = $(ele).offset().top;
+        });
+        _active($page);
+        _slide($page, $scroll);
+        _hashlisten();
+        _wheellisten();
+    };
+
+    var _active = function ($p) {
+        $('.active').removeClass('active');
+        $('.nav-li').eq($p).addClass('active');
+    };
+
+    var _slide = function ($p, $s) {
+        if (!$s) {
+            $scroll = 1;
+            return false;
+        }
+        if ($running) {
+            $('.content').stop();
+        }
+        $running = 1;
+        $('.content').animate({
+            scrollTop: $pages[$p]
+        }, 300, 'swing', function () {
+            $running = 0;
+        });
+    };
+
+    var _wheellisten = function () {
+        $(window).on('mousewheel', function () {
+            var scrollY = $('.content').scrollTop();
+            if (scrollY > $pages[$page]) {
+                if (scrollY >= $pages[$page + 1]) {
+                    ++$page;
+                    _active($page);
+                    $scroll = 0;
+                    location.hash = $page;
+                }
+            } else if (scrollY < $pages[$page]) {
+                --$page;
+                _active($page);
+                $scroll = 0;
+                location.hash = $page;
+            }
+        });
+    };
+
+    var _hashlisten = function () {
+        window.onhashchange= function () {
+            $page = isNaN(location.hash.slice(1)) ? 0 : location.hash.slice(1) * 1;
+            $page = $page >= $pagenum ? 0 : $page;
+            $('.active').removeClass('active');
+            $('.nav-li').eq($page).addClass('active');
+            _slide($page, $scroll);
+        };
+    };
+
+    return {
+        init: init
+    };
+})(window);
+
+//返回顶部
+var toTop = (function () {
+    var init = function () {
+
+        $('.top-icon').on('click', function () {
+           _top();
+        });
+    };
+
+    var _display = function () {
+        $(window).on('mousewheel', function () {
+            //$('.content').scrollTop() > $(window).height() ? '' : '' ;
+        });
+    };
+
+    var _top = function () {
+        loaction.hash = 0;
+    };
+
+    return {
+        init: init
+    };
+})();
+
+//背景转换
+var fade = (function () {
+    var $bg = 0,
+        $bgnum = 0,
+        $timer = 0;
+    var init = function (bgs) {
+        $bgnum = bgs.length;
+        _timer();
+    };
+
+    var _fadeIn = function () {
+        $('.back').append('<img class="shadow" style="display:none;" src="' + $bgs[$bg] + '">');
+        $('.back .bg').fadeOut(1500, 'swing');
+        $('.back .shadow').fadeIn(3000, 'swing', function () {
+            $('.back .bg').attr('src', $bgs[$bg]);
+            $('.back .bg').css('display', 'block');
+            $('.back .shadow').remove();
+            _timer();
+        });
+    };
+
+    var _timer = function () {
+        $timer = setTimeout(function () {
+            $bg = $bg++ >= $bgnum - 1 ? 0 : $bg;
+            _fadeIn();
+            $timer = clearTimeout();
+        }, 5000);
+    };
+
+    return {
+        init: init
+    };
+})();
+
+$(window).load(function () {
+    scrollPage.init();
+    toTop.init();
+    fade.init($bgs);
+});
