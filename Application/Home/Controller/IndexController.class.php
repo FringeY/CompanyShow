a5828396f5d5cfa4e1582db42f8f9f2ce746516b
yaorui <465964690@qq.com>
2016-02-24 23:12:46 +0800

    add modal

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,16 @@
     use Think\Controller;
     class IndexController extends Controller {
         public function index (){
+            $Article = M('articles');
+            $articles = $Article -> select();
+            foreach ($articles as $key => $val) {
+                $pattern = array("\r\n", "\r", "\n");
+                $replacement = '<br>';
+                $string = $val['content'];
+                $articles[$key]['content'] = str_replace($pattern, $replacement, $string);
+
+            }
+            $this -> assign('articles', $articles);
             $this -> display();
         }
     }
