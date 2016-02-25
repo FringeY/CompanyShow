1dfccc9fb9c079cbcd3864819d2a8b2f31b74a29
yaorui <465964690@qq.com>
2016-02-25 23:16:55 +0800

    debug

--- Application/Home/Controller/BaseController.class.php
+++ Application/Home/Controller/BaseController.class.php
@@ -5,11 +5,10 @@ class BaseController extends Controller {
     public function _initialize() {
         $id = session('user.id');
         $name = session('user.name');
-        if (!id || !name) {
+        if (!$id || !$name) {
             $this->error('请先登录', U('Login/index'));
         } else {
             $this->assign('name', $name);
-            $this->assign('id', $id);
         }
     }
 }
